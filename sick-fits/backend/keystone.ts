e2a7b2b10f7c0fccdda27662f6671ec5f4d40159
TomGower <tom.gower@gmail.com>
2021-02-10 17:50:48 -0600

    Video 14, Adding Auth to our Application

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -1,34 +1,58 @@
+/* eslint-disable arrow-body-style */
 import 'dotenv/config';
+import { createAuth } from '@keystone-next/auth';
 import { config, createSchema } from '@keystone-next/keystone/schema';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
 import { User } from './schemas/User';
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
 
 const sessionConfig = {
-  maxAge: 60 * 60 * 24 * 360, // how long should you say signed in
+  maxAge: 60 * 60 * 24 * 360, // how long should you stay signed in
   secret: process.env.COOKIE_SECRET,
 };
 
-export default config({
-  server: {
-    cors: {
-      origin: [process.env.FRONTEND_URL],
-      credentials: true,
-    },
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // TODO: add in initial roles when we get to roles
   },
-  db: {
-    adapter: 'mongoose',
-    url: databaseURL,
-    // TODO: Add data seeding here
-  },
-  lists: createSchema({
-    // schema items go in here
-    User,
-  }),
-  ui: {
-    // TODO: change this for roles
-    isAccessAllowed: () => true,
-  },
-  // TODO: add session values here
 });
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+      // TODO: Add data seeding here
+    },
+    lists: createSchema({
+      // schema items go in here
+      User,
+    }),
+    ui: {
+      // show ui only for people who pass this test
+      isAccessAllowed: ({ session }) => {
+        // console.log(session);
+        return !!session?.data;
+      }
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      // this is a GraphQL query
+      User: 'id name email',
+    }),
+  })
+);
