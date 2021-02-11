747ba931203527b83a7e9ac5f50d1ea975233b03
TomGower <tom.gower@gmail.com>
2021-02-11 16:24:18 -0600

    Video 24, Creating products via our mutations

--- sick-fits/frontend/components/CreateProduct.js
+++ sick-fits/frontend/components/CreateProduct.js
@@ -1,22 +1,67 @@
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
 import useForm from '../lib/useForm';
 import Form from './styles/Form';
+import DisplayError from './ErrorMessage';
+
+const CREATE_PRODUCT_MUTATON = gql`
+  mutation CREATE_PRODUCT_MUTATION(
+    # Which variables are getting passed in, and which types are they?
+    $name: String!
+    $description: String!
+    $price: Int!
+    $image: Upload
+  ) {
+    createProduct(
+      data: {
+        name: $name
+        description: $description
+        price: $price
+        status: "AVAILABLE"
+        # photo is its own type, so it needs to be a ProductImage
+        photo: { create: { image: $image, altText: $name } }
+      }
+    ) {
+      id
+      price
+      description
+      name
+    }
+  }
+`;
 
 export default function CreateProduct() {
   const { inputs, handleChange, resetForm, clearForm } = useForm({
     image: '',
-    name: 'nice short',
+    name: '',
     price: 12345,
     description: 'this is the best short',
   });
 
-  function handleSubmit(e) {
+  const [createProduct, { loading, error, data }] = useMutation(
+    CREATE_PRODUCT_MUTATON,
+    {
+      variables: inputs,
+    }
+  );
+
+  async function handleSubmit(e) {
     e.preventDefault();
-    console.log(inputs);
+    // console.log(inputs);
+
+    // submit the input fields to the back end;
+
+    // createProduct is defined with variables, which can be pre-known
+    // otherwise, you can pass the variables here
+    await createProduct();
+    // the response you get is the destructured {loading, error, data} from above
+    clearForm();
   }
 
   return (
     <Form onSubmit={handleSubmit}>
-      <fieldset>
+      <DisplayError error={error} />
+      <fieldset disabled={loading} aria-busy={loading}>
         <label htmlFor="image">
           Name:
           <input
@@ -33,7 +78,7 @@ export default function CreateProduct() {
             type="text"
             id="name"
             name="name"
-            placeholder="name"
+            placeholder="Name"
             value={inputs.name}
             onChange={handleChange}
           />
