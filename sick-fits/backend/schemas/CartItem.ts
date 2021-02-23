c70d7de9fa646d94e19e1359ed58b890689bc0ec
TomGower <tom.gower@gmail.com>
2021-02-23 11:18:53 -0600

    Ch 43, Cart - creating data type & 2-way relation

---
+++ sick-fits/backend/schemas/CartItem.ts
@@ -0,0 +1,19 @@
+import { integer, relationship } from '@keystone-next/fields';
+import { list } from '@keystone-next/keystone/schema';
+
+export const CartItem = list({
+  ui: {
+    listView: {
+      initialColumns: ['quantity', 'user', 'product'],
+    },
+  },
+  fields: {
+    // TODO: custom label in here
+    quantity: integer({
+      defaultValue: 1,
+      isRequired: true,
+    }),
+    product: relationship({ ref: 'Product' }),
+    user: relationship({ ref: 'User.cart' }),
+  },
+});
