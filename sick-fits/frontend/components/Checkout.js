bb88cd0255134d462b5fc4ae135466d49865515c
TomGower <tom.gower@gmail.com>
2021-02-24 12:09:54 -0600

    Ch 53, Writing client side checkout handler logic

--- sick-fits/frontend/components/Checkout.js
+++ sick-fits/frontend/components/Checkout.js
@@ -1,6 +1,13 @@
 import styled from 'styled-components';
 import { loadStripe } from '@stripe/stripe-js';
-import { CardElement, Elements } from '@stripe/react-stripe-js';
+import {
+  CardElement,
+  Elements,
+  useElements,
+  useStripe,
+} from '@stripe/react-stripe-js';
+import { useState } from 'react';
+import nProgress from 'nprogress';
 import SickButton from './styles/SickButton';
 
 const CheckoutFormStyles = styled.form`
@@ -12,20 +19,55 @@ const CheckoutFormStyles = styled.form`
   grid-gap: 1rem;
 `;
 
-const stripeLib = loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY);
+// const stripeLib = loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY);
 
-function Checkout() {
-  function handleSubmit(e) {
-    e.preventDefault();
+function CheckoutForm() {
+  const [stateError, setStateError] = useState();
+  const [loading, setLoading] = useState(false);
+  const stripe = useStripe();
+  const elements = useElements();
+  async function handleSubmit(e) {
     console.log('need to do some work to handle submit');
+    // 1. Stop the form from submitting and turn the loader on
+    e.preventDefault();
+    setLoading(true);
+    // 2. Start the page transition
+    nProgress.start();
+    // 3. Create payment method via Stripe
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: 'card',
+      card: elements.getElement(CardElement),
+    });
+    console.log('pyamentMethod', paymentMethod);
+    // 4a. Handle any errors from Stripe (CC not accepted/declined, credit limit, Discover not accepted in country, etc)
+    if (error) {
+      console.log('we got an error', error);
+      setStateError(error);
+    }
+    // 4b. Send token from successful payment to Keystone server via custom mutation
+    // 5. Change page to view the order
+    // 6. Close the cart
+    // 7. Turn the loader off
+    setLoading(false);
+    nProgress.done();
   }
 
   return (
+    <CheckoutFormStyles onSubmit={handleSubmit}>
+      {stateError && <p style={{ fontSize: 12 }}>{stateError.message}</p>}
+      <CardElement />
+      <SickButton>Check Out Now</SickButton>
+    </CheckoutFormStyles>
+  );
+}
+
+function Checkout() {
+  const [stripeLib, loadStripeLib] = useState(() =>
+    loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY)
+  );
+  return (
     <Elements stripe={stripeLib}>
-      <CheckoutFormStyles onSubmit={handleSubmit}>
-        <CardElement />
-        <SickButton>Check Out Now</SickButton>
-      </CheckoutFormStyles>
+      <CheckoutForm />
     </Elements>
   );
 }
