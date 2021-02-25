ae143147f3274ab9de4935569ac25a3e17a8a879
TomGower <tom.gower@gmail.com>
2021-02-25 11:54:55 -0600

    Ch 64, Permissions access functions

--- sick-fits/backend/access.ts
+++ sick-fits/backend/access.ts
@@ -1,8 +1,27 @@
 // folder that holds all of our access functions
 // at its simplest, access control is either yes or not based on user's session
 
+import { permissionsList } from './schemas/fields';
 import { ListAccessArgs } from './types';
 
 export function isSignedIn({ session }: ListAccessArgs) {
   return !!session;
 }
+
+const generatedPermissions = Object.fromEntries(
+  permissionsList.map((permission) => [
+    permission,
+    function ({ session }: ListAccessArgs) {
+      return !!session?.data.role?.[permission];
+    },
+  ])
+);
+
+// Permission check if someone meets a criterion, yes or no
+export const permissions = {
+  ...generatedPermissions,
+  canDance({ session }: ListAccessArgs): boolean {
+    if (!session) return true;
+    return false;
+  },
+};
